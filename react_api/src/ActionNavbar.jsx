10816a5ffc8877a7789f703d4e3ca04787f1b3fe
Dimitri TRIGALOUX <dtrigalo@student.42.fr>
2019-08-15 17:06:42 +0200

    sliding bar is difficult with no libs

--- react_api/src/ActionNavbar.jsx
+++ react_api/src/ActionNavbar.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './ActionNavbar.css'
 
-function ActionNavbar() {
-    return (
-        <div className='action-navbar'>
-            <div className='action-navbar-subdiv'>VOTE</div>
-            <div className='action-navbar-subdiv'>FAVORITES</div>
-        </div>
-    )
+class ActionNavbar extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+
+    }
+
+    render() {
+
+        return (
+            <div className='action-navbar'>
+                <a href='#vote' className='action-navbar-anchor' id='vote'>VOTE</a>
+                <a href='#fav' className='action-navbar-anchor' id='fav'>FAVORITES</a>
+                <div id='slider'></div>
+            </div>
+        )
+    }
 }
 
 export default ActionNavbar
